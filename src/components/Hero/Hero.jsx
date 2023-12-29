bf77ef45f2700f42f92f0cd0c64fe5edb7035982
LEARNER-dakshesh <daksheshgupta4@gmail.com>
2023-12-30 00:57:52 +0530

    Portfolio Website of Dakshesh Gupta

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>
+        <h1>Hii,I'm a Dakshesh</h1>
+        <p>
+          I'm a tech Enthusiast with intrest in Web And Android Technologies.
+          Reach out if you'd like to learn more!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
