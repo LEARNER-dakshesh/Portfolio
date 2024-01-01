45569696ff096a77cb367cdf750f7b834eec501e
LEARNER-dakshesh <daksheshgupta4@gmail.com>
2024-01-02 04:40:41 +0530

    updated

---
+++ src/components/About/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
+
+const About = () => {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/me-sitting.jpg")}
+          alt="Me"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursor.png")} alt="cursor-icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Fronted Developer</h3>
+              <p>
+                Frontend Developer with experience in building responsive and
+                optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/server.png")} alt="server-icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience building fast and optimized back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/mobile.png")} alt="mobile-icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Flutter Developer</h3>
+              <p>I have developed Multiple cross platform Apps using Flutter</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hii,I'm a Dakshesh</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hii, I'm a Dakshesh</h1>
+        <p className={styles.description}>
           I'm a tech Enthusiast with intrest in Web And Android Technologies.
           Reach out if you'd like to learn more!
         </p>
+        <a href="mailto:daksheshgupta4@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
+      <img
+        src={getImageUrl("hero/about-img.jpg")}
+        alt="Me"
+        className={styles.HeroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
